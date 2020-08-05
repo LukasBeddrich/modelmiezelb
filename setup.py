1569145429485f0317f06c3786b88ba2b962dfe7
HGabold <henrik.gabold@frm2.tum.de>
2020-08-05 15:47:05 +0200

    added requirements

--- setup.py
+++ setup.py
@@ -2,9 +2,10 @@ from setuptools import setup, find_packages
 
 setup(
     name="modelmiezelb",
-    version="0.1.0",
+    version="0.1.1",
     packages=find_packages(),
     author="Lukas Beddrich",
     author_email="lukas.beddrich@frm2.tum.de",
-    description="package to model intermediate scattering functions for MIEZE measurements"
-)
+    description="package to model intermediate scattering functions for MIEZE measurements",
+    install_requires=["numpy", "scipy", "skipi", "matplotlib", "pytest"]
+    )
